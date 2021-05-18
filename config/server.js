62cb0d3f5c6650100cc1dad1d134a7ce4bc1c1da
bilal1133 <bmehmood199@gmail.com>
2021-05-18 08:34:36 -0700

    changed url in server.js

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => {
     port: env.int("PORT", 1337),
     // Added by Bilal
     // url: "https://find-a-plant--server.herokuapp.com/",
-    url: "http://localhost:1337",
+    // url: "http://localhost:1337",
     admin: {
       auth: {
         secret: env("ADMIN_JWT_SECRET", "95e5408e5115bcec158a1721747e993a"),
