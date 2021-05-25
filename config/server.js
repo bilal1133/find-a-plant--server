25e04a175bd0dc589017baefaa03143292fd17ff
bilal1133 <bmehmood199@gmail.com>
2021-05-26 03:01:43 +0500

    Installed pg, aws-S3 upload provider and updated the config files

--- config/server.js
+++ config/server.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => {
+  console.log("-----", env("URL"));
   return {
     host: env("HOST", "0.0.0.0"),
     port: env.int("PORT", 1337),
     // Added by Bilal
-    url: "https://find-a-plant--server.herokuapp.com/",
+    url: env("URL", "https://find-a-plant--server.herokuapp.com/"),
     // url: "http://localhost:1337",
-
     admin: {
       auth: {
         secret: env("ADMIN_JWT_SECRET", "95e5408e5115bcec158a1721747e993a"),
