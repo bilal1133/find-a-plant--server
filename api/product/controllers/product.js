bf863e1c298c5f798cea1a984a3b1d1693d43126
bilal1133 <bmehmood199@gmail.com>
2021-06-06 13:14:04 +0500

    Added Specification compatibility reoved .env.example

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,61 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    const model = strapi.models.product;
+    if (ctx.query.specification) {
+      let query = { ...ctx.query };
+      // { specification: { pruning: '23' } }
+      delete query.specification;
+
+      let data = await strapi.query("product").find(query);
+      let a = [];
+      let keyToCompare = Object.keys(ctx.query.specification)[0];
+
+      data = data.filter((single) => {
+        return (
+          single.specification[0][keyToCompare] ===
+          ctx.query.specification[keyToCompare]
+        );
+      });
+      if (!data.length) {
+        const err = new Error();
+        err.status = 404;
+        throw err;
+        return;
+      } else return data;
+      // data.forEach((single) => {
+      //   return a.push(single.specification[0]);
+      // });
+      // return a;
+    } else {
+      return await strapi.query("product").find(ctx.query);
+    }
+  },
+
+  async findOne(ctx) {
+    try {
+      const id = ctx.req._parsedUrl.pathname.split("/products/")[1];
+      let data = await strapi.query("product").find({ _id: id });
+
+      if (!data.length) {
+        const err = new Error();
+        err.status = 404;
+        throw err;
+        return;
+      } else return data[0];
+
+      // delete data.vendor.password;
+
+      ctx.body = {
+        data,
+      };
+    } catch (error) {}
+  },
+};
